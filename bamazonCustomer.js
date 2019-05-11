4330acb0b016d134942397a69a43b4dda255a3ea
unknown <summerbasehart@gmail.com>
2019-05-11 13:36:12 -0700

    fixing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,45 +10,62 @@ const db = createConnection({
     database: 'bamazon'
 })
 
-async function getProducts ({ item_id, product_name, price, stock_quantity }) {
-    let response = await new Promise((resolve, reject) => {
-        db.query(`SELECT * FROM products`, (e, r) => {
+async function getProducts () {
+    db.query(`SELECT * FROM products`, (e, r) => {
             if (e) {
                 reject(e)
             } else {
                 resolve(r)
             }
-        })
-    })    
+        })   
     return response
 }
 
-const getAction = _ => {
-    getProducts()
-        .then (r=> {
-            prompt([
-                {
-                    type: 'list',
-                    name: 'item_id',
-                    message: 'Which item would you like to buy? (please list Item ID)',
-                    choices: r.map(({ item_id }) => ID)
-                },
-            ])
-            .then (({ item_id, choice }) => {
-                if (choice) {
-                    db.query('How many would you like to buy?', {stock_quantity}, e => {
-                        if(e) throw e
-                        console.log('Purchase successful!')
-                        getAction()
-                    })
-                } else {
-                    getAction()
-                }
-            })
-            .catch(e => console.log(e))
-                
-        })
-        .catch(e => console.log(e))
+var choices = {
+    message: "Pick an item..",
+    type: "list",
+    name: "items",
+    choices: r.map(( {item_id} ) => ID)
+}
+    
+var bids = {
+    message: "How Many?",
+    type: "input",
+    name: "quantity",
+    when : function( answers ) {
+            return answers.feedback === "Bid on item";
+          },
+    default: db.query('SELECT  FROM products', (e, data) => {
+        if (e) {console.log(e)}
+        console.log(data)
 }
 
-db.connect(e => e ? console.log(e) : getProducts())
+// const getAction = _ => {
+//     getProducts()
+//         .then (r=> {
+//             prompt([
+//                 {
+//                     type: 'list',
+//                     name: 'item_id',
+//                     message: 'Which item would you like to buy? (please list Item ID)',
+//                     choices: r.map(({ item_id }) => ID)
+//                 },
+//             ])
+//             .then (({ item_id, choice }) => {
+//                 if (choice) {
+//                     db.query('How many would you like to buy?', {stock_quantity}, e => {
+//                         if(e) throw e
+//                         console.log('Purchase successful!')
+//                         getAction()
+//                     })
+//                 } else {
+//                     getAction()
+//                 }
+//             })
+//             .catch(e => console.log(e))
+                
+//         })
+//         .catch(e => console.log(e))
+// }
+
+// db.connect(e => e ? console.log(e) : getProducts())
